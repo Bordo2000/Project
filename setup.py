dd7907f5586f9c7f37f4a81ce8ba1dcb2dec220f
Bordo2000 <132795397+Bordo2000@users.noreply.github.com>
2023-05-07 04:02:13 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='mega',
+    version='1.0.8',
+    description='mega.py for tenacity 8.2.2 ',
+    author='Bordo200',
+    author_email='bordo1337@gmail.com',
+    url='https://github.com/Bordo2000/Project',
+    packages=['mega'],
+    install_requires=[
+        'tenacity==8.2.2',
+    ]
+)
